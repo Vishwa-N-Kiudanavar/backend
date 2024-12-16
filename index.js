de9d9f3616733c72cdbd72b1121f9db0cd4d9547
vkiudanavar@gmail.com <vkiudanavar@gmail.com>
2024-12-16 18:14:41 +0530

    all for cors policy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:3000',
+    origin:'https://frontend-uh5o.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
